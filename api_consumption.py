61219ca715d1334edb37406af0c78c90e19367c2
makena <roselynemakena@gmail.com>
2017-02-08 19:01:26 +0300

    code comments cleanup on api consumption

--- api_consumption.py
+++ api_consumption.py
@@ -1,29 +1,21 @@
 import requests
-
 base_url = 'http://api.openweathermap.org/data/2.5/weather'
 api_key='008609466db81f5f971a7ff1e0c14774'
-
 def get_weather_by_city(city):
 	query = base_url+'?q=%s&units=metric&APPID=%s'%(city,api_key)
-	# print(query)
-	# print("**********")
 	try:
 		res = requests.get(query)
 		print("*******success*****")
-		# print("[%s] %s" % (res.status_code, res.url))
 		if(res.status_code !=200):
 			return 'City not found'
 		else:
 			weather_info = res.json()
 			return weather_info
-
 	except requests.exceptions.RequestException as error:
     		print(error)
 	except:
-		#raise error
 		print("Some Exception error")
 		raise Exception("**********************")
-
 	return
 		#errror or data
 
