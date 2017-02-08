aff231b242218b059cf064b9719f241c18db4d1d
makena <roselynemakena@gmail.com>
2017-02-08 16:23:50 +0300

    added api consumption for all weather

--- real_world.py
+++ real_world.py
@@ -8,7 +8,7 @@ class Project(object):
     self.project_description = project_description
 
 
-
+#Inheritance of Project into User module
 class User(Project):
   """docstring for User"""
   def __init__(self, first_name, last_name):
