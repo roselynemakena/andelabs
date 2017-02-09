2dc5bcaba2dd4b1c94ddf30cd5c78749caf4936b
makena <roselynemakena@gmail.com>
2017-02-09 19:30:04 +0300

    add list_difference.py

---
+++ list_difference.py
@@ -0,0 +1,9 @@
+
+def find_missing(list1, list2):
+  if list1==[] or list2==[]:
+    return 0
+  diff = list(set(list1).symmetric_difference(set(list2)))
+  return diff
+
+r = find_missing([1,2,3],[1,2,3,4])
+print(r)
